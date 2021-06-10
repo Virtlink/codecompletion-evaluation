d25ab22ee1cc505647888e65ec63049f85a81b49
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2021-06-10 15:22:36 +0200

    Privileged Docker run

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ set -o xtrace
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 docker image build -t codecompletion-eval ${DIR}
-docker container run -it --rm \
+docker container run --privileged -it --rm \
   -v ${DIR}/output-det:/home/output-det \
   -e OUTPUT=/home/output-det \
   codecompletion-eval \
